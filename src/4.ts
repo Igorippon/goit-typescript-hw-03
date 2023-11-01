46a17cfab26ead68909c7d980a845272a2fb8937
Igorippon <botvunaigor@gmail.com>
2023-11-01 20:08:13 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,57 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public key: Key;
+  public tenants: Person[] = [];
+
+  comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+
+  abstract OpenDoor(obj: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  OpenDoor(obj: Key): void {
+    if (obj.getSignature === this.key.getSignature) {
+      this.door = true;
+      console.log("Open");
+    } else {
+      console.log("Closed");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
